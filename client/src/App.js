b5c42ce50204902e070a54157eb5a0783a05c51b
ArturVargas <arturvargas9226@gmail.com>
2020-06-02 08:31:10 -0500

    create navbar and avatar components

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Voting from "./contracts/Voting.json";
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 class App extends Component {
   state = { storageValue: 0, web3: null, accounts: null, contract: null };
@@ -35,36 +36,13 @@ class App extends Component {
     }
   };
 
-  // runExample = async () => {
-  //   const { accounts, contract } = this.state;
-
-  //   // Stores a given value, 5 by default.
-  //   // await contract.methods.set(5).send({ from: accounts[0] });
-
-  //   // Get the value from the contract to prove it worked.
-  //   // const response = await contract.methods.get().call();
-
-  //   // Update state with the result.
-  //   this.setState({ storageValue: response });
-  // };
-
   render() {
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <Navbar />
       </div>
     );
   }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
-// import Avatar;
+import VoteLogo from '../icons/Vote';
+import Avatar from './Avatar';
 
-export const Navbar = () => (
+const Navbar = ({ account }) => (
   <React.Fragment>
+    <nav className="navbar fixed-top navbar-expand-md navbar-dark bg-dark">
+      <a className="navbar-brand" href="/">
+        <img src={ VoteLogo } width="50" height="auto" className="d-inline-block align-top" alt="" />
+          TwitChain
+      </a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item active">
+            <span className="font-weight-bolder text-white mr-2">
+              {account}
+            </span>
+            {
+              account && (
+                <Avatar userAddress={account} />
+              )
+            }
+          </li>
 
+        </ul>
+      </div>
+    </nav>
   </React.Fragment>
-)
+);
+
+export default Navbar;
